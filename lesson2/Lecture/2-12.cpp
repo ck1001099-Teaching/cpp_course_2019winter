ee015a96bfd9d7afab881f283bfc79a82b9d1cef
CHENYI <r07922059@ntu.edu.tw>
2019-01-16 11:12:10 +0800

    新增lesson2上課教材

---
+++ lesson2/Lecture/2-12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	for (int i = 0 ; i < 10 ; i++){ // 計數器
+		cout << i << endl;
+	}
+
+	// i++  :  i = i + 1
+
+	for (int j = 0 ; j < 10 ; j++){
+		cout << j << endl;
+		j = j + 1;
+	}
+
+
+	return 0;
+}
